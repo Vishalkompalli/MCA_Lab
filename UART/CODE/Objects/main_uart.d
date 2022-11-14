2e5f5911779c7a6a212e5642f8bacb8c3e9bbd42
Vishalkompalli <vishal.kompalli@gmail.com>
2022-11-14 11:43:17 +0530

    PLL, Timer/Counter, UART added

---
+++ UART/CODE/Objects/main_uart.d
@@ -0,0 +1,4 @@
+.\objects\main_uart.o: main_uart.c
+.\objects\main_uart.o: pll.h
+.\objects\main_uart.o: uart.h
+.\objects\main_uart.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
---
+++ UART/CODE/pll.h
@@ -0,0 +1,2 @@
+void init_pll(void);
+

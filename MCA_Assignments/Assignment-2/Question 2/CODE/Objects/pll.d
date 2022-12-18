72a4cb3988d26c51aba699c4af34abd44858b9e5
Vishalkompalli <vishal.kompalli@gmail.com>
2022-12-18 18:18:07 +0530

    Updated

---
+++ MCA_Assignments/Assignment-2/Question 2/CODE/Objects/pll.d
@@ -0,0 +1,3 @@
+.\objects\pll.o: ..\..\..\Assignment-1\Ques1\CODE\pll.c
+.\objects\pll.o: ..\..\..\Assignment-1\Ques1\CODE\pll.h
+.\objects\pll.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
---
+++ MCA_Assignments/Assignment-2/Question 2/CODE/Objects/timer.d
@@ -0,0 +1,3 @@
+.\objects\timer.o: ..\..\..\Assignment-1\Ques1\CODE\timer.c
+.\objects\timer.o: ..\..\..\Assignment-1\Ques1\CODE\timer.h
+.\objects\timer.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
